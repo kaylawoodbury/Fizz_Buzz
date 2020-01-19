af0303733dc50c9955aa92d9ca8ea69a7bf46a85
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-19 12:04:28 +0100

    adding third test for if number is divisible by 5

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizz_buzz' do
    it 'returns fizz if number is divisible by 3' do
      expect(fizz_buzz(3)).to eq 'Fizz'
     end
+
+    it 'returns buzz if number is divisible by 5' do
+     expect(fizz_buzz(5)).to eq 'Buzz'
+    end
 end
